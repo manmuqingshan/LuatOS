ac89e4dd4b99964f2874d8dd59c18c88a00ed16d
陈取德 <chenqude@airm2m.com>
2026-05-20 11:39:41 +0800

    修改：修改8101系列AirCAMERA_1020\1030\1031的应用DEMO，readme中增加photo_to_aircloud.lua的应用描述。

--- module/Air8101/demo/accessory_board/AirCAMERA_1020/main.lua
+++ module/Air8101/demo/accessory_board/AirCAMERA_1020/main.lua
@@ -62,12 +62,12 @@ require "netdrv_wifi"
 
 -- 以下录像和拍照演示，只能二选一打开
 -- 导入take_photo_http_post拍照上传应用DEMO
-require "take_photo_http_post"
+-- require "take_photo_http_post"
 
 -- 导入video_http_post视频上传应用DEMO
 -- require "video_http_post"
 
-
+require "photo_to_aircloud"
 -- 用户代码已结束---------------------------------------------
 -- 结尾总是这一句
 sys.run()
