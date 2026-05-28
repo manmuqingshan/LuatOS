0ce5e62f5f60f096cf0a29277d106f3689cdf19c
Wendal Chen <wendal1985@gmail.com>
2026-05-28 22:05:06 +0800

    change: fs,vfs允许同一个文件系统驱动重复加载,并加大默认文件系统驱动数量,默认可挂载文件系统数量,最大打开文件数量

--- luat/vfs/luat_vfs.c
+++ luat/vfs/luat_vfs.c
@@ -38,12 +38,12 @@ int luat_vfs_init(void* params) {
 int luat_vfs_reg(const struct luat_vfs_filesystem* fs) {
     for (size_t i = 0; i < LUAT_VFS_FILESYSTEM_MAX; i++)
     {
+        if (vfs.fsList[i] == fs) {
+            return 0;
+        }
         if (vfs.fsList[i] == NULL) {
             vfs.fsList[i] = (struct luat_vfs_filesystem*)fs;
             //LLOGD("register fs %s", fs->name);
-            return 0;
-        }
-        if (vfs.fsList[i] == fs) {
             return 0;
         }
     }
